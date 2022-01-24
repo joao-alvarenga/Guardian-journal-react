d0e4348896681d530207438ee4b7a612b075035e
João Alvarenga <jvalvarenga@mailfence.com>
2022-01-24 14:16:41 -0300

    add nav and nav-links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,36 +1,80 @@
 import React from "react";
 import Button from "../button/Button";
-import { MainHeader, MainNav } from "./Header.styles";
+import { MainHeader, HeaderWrap, LogoContainer, Nav } from "./Header.styles";
 // Icons
 import { AiOutlineArrowRight } from "react-icons/ai";
 // SCSS modules
 import styles from "./styles.module.scss";
+// Images
+import LogoImage from "../../images/TheGuardianLogo.png";
+// Links
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
       <MainHeader>
-        <MainNav>
-          <h2>Support the Guardian</h2>
-          <h4>Available for everyone, funded by readers</h4>
-          <div style={{ marginBlockStart: "0.6rem" }}>
-            <Button
-              className={styles.linkButton}
-              style={{ marginInlineEnd: "1rem" }}
-              type="link"
-              href="https://github.com/joao-alvarenga"
-            >
-              GitHub <AiOutlineArrowRight className={styles.arrowIcon} />
-            </Button>
-            <Button
-              className={styles.linkButton}
-              type="link"
-              href="https://www.linkedin.com/in/jo%C3%A3o-alvarenga/"
-            >
-              LinkedIn <AiOutlineArrowRight className={styles.arrowIcon} />
-            </Button>
+        <HeaderWrap>
+          <div>
+            <h2>Support the Guardian</h2>
+            <h4>Available for everyone, funded by readers</h4>
+            <div style={{ marginBlockStart: "0.6rem" }}>
+              <Button
+                className={styles.linkButton}
+                style={{ marginInlineEnd: "1rem" }}
+                type="link"
+                href="https://github.com/joao-alvarenga"
+              >
+                GitHub <AiOutlineArrowRight className={styles.arrowIcon} />
+              </Button>
+              <Button
+                className={styles.linkButton}
+                type="link"
+                href="https://www.linkedin.com/in/jo%C3%A3o-alvarenga/"
+              >
+                LinkedIn <AiOutlineArrowRight className={styles.arrowIcon} />
+              </Button>
+            </div>
           </div>
-        </MainNav>
+          <LogoContainer>
+            <img
+              src={LogoImage}
+              alt="The Guardian logo"
+              className={styles.logo}
+            />
+          </LogoContainer>
+        </HeaderWrap>
+        <Nav>
+          <nav>
+            <ul>
+              <li>
+                <Link className={styles.navLink} to="">
+                  News
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.navLink} to="">
+                  Opinion
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.navLink} to="">
+                  Sport
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.navLink} to="">
+                  Culture
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.navLink} to="">
+                  Lifestyle
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </Nav>
       </MainHeader>
     </div>
   );
--- src/components/header/Header.styles.js
+++ src/components/header/Header.styles.js
@@ -6,14 +6,15 @@ export const MainHeader = styled.header`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   background-color: #052962;
 `;
 
-export const MainNav = styled.div`
+export const HeaderWrap = styled.div`
   width: 85%;
-  height: 100%;
-  background-color: #f20;
-
+  height: 80%;
+  position: relative;
+  display: flex;
   h2 {
     font-size: 3.4rem;
     font-weight: 900;
@@ -26,5 +27,52 @@ export const MainNav = styled.div`
     color: #ffffff;
     margin-block-start: 0.31rem;
     font-family: "Open Sans", sans-serif;
+  }
+`;
+
+export const LogoContainer = styled.div`
+  /* position: absolute; */
+  /* right: 3rem; */
+  margin-left: auto;
+  margin-top: 1rem;
+  background-size: cover;
+  object-fit: cover;
+  background-position: center;
+`;
+
+export const Nav = styled.div`
+  display: flex;
+  width: 85%;
+  height: 4.5rem;
+  border: 1px solid #506991;
+  border-bottom: none;
+  margin-top: auto;
+
+  ul {
+    list-style: none;
+    /* display: flex; */
+    /* align-items: center;
+    gap: 8rem; */
+  }
+
+  ul:first-of-type {
+    padding-left: 0.8rem;
+  }
+
+  li {
+    font-size: 2.1rem;
+    font-weight: 900;
+    float: left;
+    display: block;
+    width: 13vw;
+    display: flex;
+    position: relative;
+  }
+
+  li:not(:first-child):before {
+    content: "";
+    position: absolute;
+    border-left: 1px solid #506991;
+    height: 83%;
   }
 `;
